c511117928acd9953c5a6caec2c3d10552b1e14d
ParimalNirpagare <parimal.nirpagare@gmail.com>
2026-03-11 00:44:47 +0530

    Assignment1 Submission

---
+++ assignments/assignment1.ts
@@ -0,0 +1,46 @@
+let tempOfCity: number = 25.5;
+console.log(tempOfCity);
+let customerOrder: boolean = true;
+console.log(customerOrder);
+let phoneNumber: number = 25.5; 
+console.log(phoneNumber);
+let amountCustAccount: number = 1000.50;
+console.log(amountCustAccount);
+let personEmailAddress : string = "john.doe@example.com";
+console.log(personEmailAddress);
+let locationCoordinate : number[]    = [37.7749,-122.4194];
+console.log(locationCoordinate);
+let martialStatus: boolean = true;
+console.log(martialStatus);
+let personOccupation : string = "Sofware Engineer";
+console.log(personOccupation);
+let peronsFavColor : string = "Blue";
+console.log(peronsFavColor);
+let currentYear: number = 2023; 
+console.log(currentYear);
+let socialMediaFollowers : number = 1000000;
+console.log(socialMediaFollowers);
+let ratingOfMovie : number = 7.5;
+console.log(ratingOfMovie);
+let personsBloodType : string = "'A'"
+console.log(personsBloodType);
+let bookTitle : string = "To Kill a Mockingbird";
+console.log(bookTitle);
+let numberOfEmp : number = 500;
+console.log(numberOfEmp);
+let eventTime : string = "2:30PM";
+console.log(eventTime);
+let contryNmae : string = "United States"
+console.log(contryNmae);
+let personEyeColour : string = "Brown"
+console.log(personEyeColour);
+let personBirthPlace : string = "New York City";
+console.log(personBirthPlace);
+let distanceBetween : number = 200.5;
+console.log(distanceBetween);
+
+
+
+
+
+
